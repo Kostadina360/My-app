17da9535d94fd2813866a452625bc2b4f7fcbb06
Kostadina <krmaslarova@gmail.com>
2019-08-16 21:34:31 +0200

    add switch

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import logIn from './LogInComponents/logIn'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Menu from './LogInComponents/Menu'
+
 
 
 const Routes =() => {
 return (
     <Router>
         <Menu/>
-        <Route exact={true} path='/' component={null}/>
-        <Route exact={true} path='/login' component={logIn}/>
+        <Switch>
+            <Route exact path='/' component={null}/>
+            <Route exact path='/login' component={logIn}/>
+            <Route exact component={PathError}/>
+        </Switch>
 
     </Router>
 )
 }
 
 export default Routes
+
+const PathError =()=>{
+
+    return <h1>Error 404! Not found.</h1>
+}
